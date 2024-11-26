1af4be831130a305c32837fc4ffa129ef4a72d40
Chris Kenny <159801689+Chriswkenny@users.noreply.github.com>
2024-11-26 09:52:10 -0500

    CS175 Lab Q1.py

--- CS175 Lab Q1.py
+++ CS175 Lab Q1.py
@@ -1,4 +1,7 @@
+#Christopher Kenny
+#CS175
 #Q1: WHAT WILL THE FOLLOWING CODE RETURN WHEN YOU CALL THE FUNCTION
+
 def rectangle_area(length, width=5):
   return length*width
 print(rectangle_area(10, width=7))
